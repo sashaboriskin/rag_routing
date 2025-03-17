3cf48d3c1d1db2ddbbf70a7010eca00c513b52e7
sashaboriskin <brskn.lx@gmail.com>
2025-03-17 15:33:33 +0300

    Remove refusing from prompts

--- prompts.py
+++ prompts.py
@@ -2,10 +2,10 @@ def w_context_user_prompt(question, context):
     return f'Context: "{context}" Question: "{question}"'
 
 def w_context_system_prompt():
-    return 'You are a helpful assistant. Provide extremely short and factually correct answers based only on the given context. If the context does not contain relevant information, respond with "I do not know". Limit your answer to just a few words. Avoid explanations or additional details.'
+    return 'You are a helpful assistant. Provide extremely short and factually correct answers based only on the given context. Limit your answer to just a few words. Avoid explanations or additional details.'
 
 def wo_context_system_prompt():
-    return 'You are a helpful assistant. Your task is to provide extremely short and factual answers. Respond with just the key fact, name, date, or relevant information in no more than a few words. Avoid any explanations, context, or additional details.'
+    return 'You are a helpful assistant. Provide extremely short and factual answers. Limit your answer to just a few words. Respond with just the key fact, name, date, or relevant information in no more than a few words. Avoid any explanations, context, or additional details.'
 
 def correctness_system_prompt():
     return """You will be given a user query, a reference answer, and a model's response. Your task is to evaluate the model’s response only in relation to the reference answer. Focus solely on the accuracy and completeness of the information in the model’s response compared to the reference. Ignore stylistic differences, minor rephrasings, or formatting.
